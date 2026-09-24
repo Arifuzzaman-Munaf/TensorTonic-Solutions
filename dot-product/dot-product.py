e5a5689703517f31d0505167d44afd22c4207e07
Md. Arifuzzaman Munaf <66621433+Arifuzzaman-Munaf@users.noreply.github.com>
2026-09-24 17:56:25 +1000

    Solve dot-product

--- dot-product/dot-product.py
+++ dot-product/dot-product.py
@@ -5,7 +5,4 @@ def dot_product(x: list, y: list) -> float:
     Returns the dot product as a float.
     """
     # Write code here
-    product = 0.0
-    for a, b in zip(x,y):
-        product += a * b
-    return product
+    return float(np.dot(np.asarray(x), np.asarray(y)))
